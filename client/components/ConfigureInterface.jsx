a8b30ee08da074c4a9bbf35b838c16a26ea97334
EH1537 <ehilton1537@gmail.com>
2020-01-01 17:59:29 -0800

    some styling and formatting complete on clien, need to work on UPDATE

--- client/components/ConfigureInterface.jsx
+++ client/components/ConfigureInterface.jsx
@@ -10,36 +10,48 @@ const ConfigureInterface = (props) => {
   return (
     <div className="configInterface">
       <form onSubmit={handleSubmit}>
+      <div className = "inputComponent">
+        <label for="IPname">Name of Interface</label><br></br>
         <input
           id = "IPname"
           type="text"
-          placeholder="Name of Interface"
+          placeholder="ex: fa0/1 or g0/0"
           onChange={props.interfaceConfig}
           value={props.interfaceName}>
         </input>
+        </div>
+        <div className = "inputComponent">
+        <label for="description">Interface desciption</label><br></br>
         <input
           id = "description"
           type="text"
-          placeholder="task description"
+          placeholder="where it goes"
           onChange={props.interfaceConfig}
           value={props.interfaceDescription}>
         </input>
+        </div>
+        <div className = "inputComponent">
+        <label for="address">IP Address</label><br></br>
         <input
           id = "address"
           type="text"
-          placeholder="ip address"
+          placeholder="Address at interface"
           onChange={props.interfaceConfig}
           value={props.interfaceAddress}>
         </input>
+        </div>
+        <div className = "inputComponent">
+        <label for="address">IP Address</label><br></br>
         <input
           id = "subnet"
           type="text"
-          placeholder="subnet mask"
+          placeholder="Use 255.255.255.255 format"
           onChange={props.interfaceConfig}
           value={props.interfaceSubnet}>
         </input>
-        <br></br>
-        <input type="submit" value="Generate Config"></input>
+        </div>
+        <div class="break"></div> 
+        <input class= "GenerateConfig" type= "submit" value="Generate Config"></input>
       </form>
       <ConfigurationDisplay display={(props.display)}/>
     </div>
--- client/components/CreateDevice.jsx
+++ client/components/CreateDevice.jsx
@@ -9,89 +9,121 @@ const CreateDevice = (props) => {
 
   return (
     <div className="deviceCreator">
-      <form onSubmit={handleCreateSubmit}>
-        <input
+      <form onSubmit={handleCreateSubmit}><br></br>
+        <div className = "inputComponent">
+        <label for="nameString">Name of Device</label><br></br>
+          <input
           id="nameString"
           type="text"
-          placeholder="Name of Device"
+          placeholder="Needs to be unique"
           onChange={props.deviceConfig}
           value={props.nameString}>
         </input>
+        </div>
+        <div className = "inputComponent">
+        <label for="secretString">Secret Password</label><br></br>
         <input
           id="secretString"
           type="text"
-          placeholder="Secret Password"
+          placeholder="For executive privilege"
           onChange={props.deviceConfig}
           value={props.secretString}>
         </input>
+        </div>
+        <div className = "inputComponent">
+        <label for="conString">Console Password</label><br></br>
         <input
           id="conString"
           type="text"
-          placeholder="Console Password"
+          placeholder="For console access"
           onChange={props.deviceConfig}
           value={props.conString}>
         </input>
+        </div>
+        <div className = "inputComponent">
+        <label for="vtyString">VTY Password</label><br></br>
         <input
           id="vtyString"
           type="text"
-          placeholder="VTY Password"
+          placeholder="For remote access"
           onChange={props.deviceConfig}
           value={props.vtyString}>
         </input>
+        </div>
+        <div className = "inputComponent">
+        <label for="bannerInString">Banner MOTD</label><br></br>
         <input
           id="bannerInString"
           type="text"
-          placeholder="Banner Text (leave blank for default)"
+          placeholder="Leave blank for canned"
           onChange={props.deviceConfig}
           value={props.bannerInString}>
         </input>
-        <br></br>
+        </div>
+        <div class="break"></div> 
+        <div>
         <button id='RouterOrSwitch' type='button' onClick={() => props.routerOrSwitchToggle()}>Router?</button>
-        <br></br>
+        </div>
+        <div class="break"></div> 
         {props.RouterOrSwitch === false &&
+          <div className="RorS" >
           <div>
+          <label for="vlanNumString">VLAN Number</label><br></br>
             <input
               id='vlanNumString'
               type="text"
-              placeholder="VLan Number"
+              placeholder="1 for remote access"
               onChange={props.deviceConfig}
               value={props.vlanNumString}>
             </input>
+          </div>
+          <div>
+          <label for="vlanIPString">VLAN IP Address</label><br></br>
             <input
               id="vlanIPString"
               type="text"
-              placeholder="VLan IP Address"
+              placeholder="Switches Address"
               onChange={props.deviceConfig}
               value={props.vlanIPString}>
             </input>
+          </div>
+          <div>
+          <label for="vlanSubString">VLAN Subnet Mask</label><br></br>
             <input
               id="vlanSubString"
               type="text"
-              placeholder="VLan Subnet Mask"
+              placeholder="Use 255.255.255.0 format"
               onChange={props.deviceConfig}
               value={props.vlanSubString}>
             </input>
+          </div>
+          <div>
+          <label for="switchGateString">Default Gateway for Switch</label><br></br>
             <input
               id="switchGateString"
               type="text"
-              placeholder="Default Gateway for Switch"
+              placeholder="The address of router"
               onChange={props.deviceConfig}
               value={props.switchGateString}>
             </input>
           </div>
+          </div>
         }
         {props.RouterOrSwitch === true && <div>
+          <div className = "inputComponent">
+          <label for="auxString">Aux Password for Router</label><br></br>
           <input
           id="auxString"
           type="text"
-          placeholder="Aux Password for Router"
+          placeholder="For auxiliary access"
           onChange={props.deviceConfig}
           value={props.auxString}>
           </input>
+          </div>
         </div>}
-        <br></br>
-        <button id='GenerateConfig' type='button' onClick={() => props.generateConfig()}>Generate Config</button>
-        <br></br>
+        <div class="break"></div> 
+        <button class='GenerateConfig' type='button' onClick={() => props.generateConfig()}>Generate Config</button>
+        <div class="break"></div> 
         <input id='addDevice' type="submit" value="Add Device"></input>
       </form>
       <ConfigurationDisplay display={(props.display)} />
